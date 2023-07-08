15442a0b9b5040278a2797b649d8bb934666a4b1
Luciana-Motta <70346081+Luciana-Motta@users.noreply.github.com>
2023-07-08 20:28:19 -0300

    Update Estado.h

--- Estado.h
+++ Estado.h
@@ -8,16 +8,33 @@ class Estado {
 private:
     std::vector<Jarro> jarros;
     std::vector<std::vector<int>> movimentos;
+    Estado *pai; 
+    std::vector<Estado> filhos;
+    std::vector<Estado> abertos;
     bool visitado;
     int numero_movimentos;
+    int heuristica;
 
 public:
-    Estado(const std::vector<Jarro> &jarros);
+    int solucao; 
+
+    Estado(const std::vector<Jarro> &jarros, int sol);
     void defineMovimentos(const std::vector<std::vector<int>> &movs);
     void marcarComoVisitado();
     void defineNumeroMovimentos(int num_movimento);
     std::vector<Jarro> getJarros();
     int getNumMovimentos();
+    void heuristicaCalculo();
+    int get_heuristica();
+    bool haSolucao();
+    void setPai(Estado* estadoPai);
+    Estado getPai() const;
+    void setFilhos(std::vector<Estado> estadosFilhos);
+    std::vector<Estado> getFilhos() const;
+    void setAbertos(std::vector<Estado> estadosAbertos);
+    std::vector<Estado> getAbertos() const;
+    bool operator==(Estado& outro) const;
+    
 
 };
 
