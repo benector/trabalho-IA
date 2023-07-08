fa758c42fe300a8ad16e9eef962c1182f36d86de
Luciana-Motta <70346081+Luciana-Motta@users.noreply.github.com>
2023-07-08 20:27:59 -0300

    Update Estado.cpp

--- Estado.cpp
+++ Estado.cpp
@@ -2,12 +2,32 @@
 #include "Estado.h"
 #include "Jarro.h"
 
+
+
+
 // Construtor
-Estado::Estado(const std::vector<Jarro> &lista_jarros) 
+Estado::Estado(const std::vector<Jarro> &lista_jarros, int sol) 
 {
     jarros = lista_jarros;
+    solucao = sol; 
     visitado = false;
     numero_movimentos = 0;
+    abertos = std::vector<Estado>();
+
+}
+
+bool Estado::operator==(Estado& outro) const {
+
+    for (size_t i = 0; i < jarros.size(); ++i) {
+        if (jarros[i].get_conteudo() != outro.jarros[i].get_conteudo()) {
+            return false; // Quantidades diferentes nos jarros, estados não são iguais
+        }
+    }
+
+    if(numero_movimentos != outro.getNumMovimentos())
+        return false;
+
+    return true; // Todos os jarros têm as mesmas quantidades, estados são iguais
 }
 
 void Estado::defineMovimentos(const std::vector<std::vector<int>> &movs)
@@ -32,4 +52,60 @@ std::vector<Jarro> Estado::getJarros()
 int Estado::getNumMovimentos()
 {
     return numero_movimentos;
+}
+
+// Função para calcular a heurística
+void Estado::heuristicaCalculo() 
+{
+    int soma = 0;
+    for (const auto& jarro : jarros) {
+        soma += std::abs(jarro.get_conteudo() - solucao);
+    }
+    heuristica = soma;
+}
+
+// Função retorna a heurística
+int Estado::get_heuristica() 
+{
+    return heuristica;
+}
+
+bool Estado::haSolucao()
+{
+    // cout << "Verifica solucao no estado atual " <<endl;
+    // imprimeJarros(jarros);
+    for (int i = 0; i < jarros.size(); i++)
+    {
+        if (jarros[i].get_conteudo() == solucao)
+        {
+            // cout << "Ha solucao no estado atual" <<endl;
+            return true;
+        }
+    }
+    // cout << "Não ha solucao no estado atual" <<endl;
+    return false;
+}
+
+void Estado::setPai(Estado* estadoPai) {
+    pai = estadoPai;
+}
+
+Estado Estado::getPai() const {
+    return *pai;
+}
+
+void Estado::setFilhos(std::vector<Estado> estadosFilhos) {
+    filhos = estadosFilhos;
+}
+
+std::vector<Estado> Estado::getFilhos() const {
+    return filhos;
+}
+
+void Estado::setAbertos(std::vector<Estado> estadosAbertos) {
+    abertos = estadosAbertos;
+}
+
+std::vector<Estado> Estado::getAbertos() const {
+    return abertos;
 }
